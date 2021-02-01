c033cd1efb45fba438822f05263c847a438310bb
Daniel Kappe <daniel.kappe@fh-bielefeld.de>
2021-02-01 09:13:04 +0100

    Adds the zsh configuration
    Features:
     * powerline 10k Theme
     * fzf Plugin mit (ripgrep, fd-find, silversearch-ag)
     * zsh-autosuggestions
     * zsh-aliases

---
+++ .oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,41 @@
+# Set personal aliases, overriding those provided by oh-my-zsh libs,
+# plugins, and themes. Aliases can be placed here, though oh-my-zsh
+# users are encouraged to define aliases within the ZSH_CUSTOM folder.
+# For a full list of active aliases, run `alias`.
+#
+# Example aliases
+alias zshconfig="nvim $HOME/.zshrc"
+alias ohmyzsh="nvim $HOME/.oh-my-zsh"
+alias zshalias="nvim $HOME/.oh-my-zsh/custom/aliases.zsh"
+alias zshsource="source $HOME/.zshrc"
+
+# Suffix aliases
+alias -g ecs-dev="dkappe@ecs-dev"
+alias -g ilias-dev="dkappe@ilias-dev"
+alias -g ecs-test="dvz@ecs-test.fh-bielefeld.de"
+alias -g ecs="dvz@ecs.fh-bielefeld.de"
+
+if [ -x /usr/bin/dircolors ]; then
+    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+    alias ls='ls --color=auto'
+    #alias dir='dir --color=auto'
+    #alias vdir='vdir --color=auto'
+
+    alias grep='grep --color=auto'
+    alias fgrep='fgrep --color=auto'
+    alias egrep='egrep --color=auto'
+fi
+
+# colored GCC warnings and errors
+#export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
+
+# some more ls aliases
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+
+# Add an "alert" alias for long running commands.  Use like so:
+#   sleep 10; alert
+alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
+
+
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # zshconfig
+My custom zsh configuration. Install and enjoy.
+
+## Installation
+Copy the content into your home directory:
+```sh
+cp . ~/
+```
+Pull zsh-autosuggestions:
+```sh
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+```
+Install the Sauce Code Pro Semibold Nerd Font Mono:
+```sh
+wget https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/SourceCodePro/Semibold/complete/Sauce%20Code%20Pro%20Semibold%20Nerd%20Font%20Complete%20Mono.ttf --directory-prefix=~/.local/share/fonts
+```
+Install fuzzy search:
+```sh
+sudo apt install ripgrep fd-find silversearcher-ag
+```
